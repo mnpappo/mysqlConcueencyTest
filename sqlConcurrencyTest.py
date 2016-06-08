d3c03aed636b6cf68b11248a9fa38feb3ff25e80
Nadimozzaman Pappo <mnpappo@gmail.com>
2016-06-08 06:17:52 +0600

    result from sql added

--- sqlConcurrencyTest.py
+++ sqlConcurrencyTest.py
@@ -4,17 +4,34 @@ import sys
 db = MySQLdb.connect("127.0.0.1", "root", "0007", "TESTDB")
 cursor = db.cursor()
 
+def iter_row(cursor, size=10):
+    while True:
+        rows = cursor.fetchmany(size)
+        if not rows:
+            break
+        for row in rows:
+            yield row
 
 def crossProductTest():
-    query = "select * from tableA,tableB"
+    query = "select * from tableB, tableA LIMIT 0, 150000"
     try:
         rows = cursor.execute(query)
-        data = cursor.fetchall()
-        counter = cursor.rowcount
 
-        print "Total %d rows got!" % counter
+        for row in iter_row(cursor, 150000):
+            print(row)
+
+        # data = cursor.fetchall()
+
+        # statement = cursor.statement
+
+        counter = cursor.rowcount
+        print "Total %s rows got!" % counter
+        # print "The statement is: %s" % statement
+
+        print "Cross Product Test successfull"
 
     except:
+        print "Error!!!"
         print sys.exc_info()[0]
 
     # for row in data:
@@ -34,7 +51,6 @@ def joinTest():
 
 
 crossProductTest()
-print "Cross Product Test successfull"
 
 
 # joinTest()
